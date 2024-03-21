c702b475f6583db3d07e3b8b64f474eea001d28e
Sonny Sasaka <sonnysasaka@gmail.com>
2024-03-21 11:54:02 -0700

    Test PHP_FPM_WORKER_COUNT

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
+# Build builder image from https://github.com/GoogleCloudPlatform/buildpacks first:
+# $ bazel build builders/php/builder.image
+# Check that local docker image gcp/php is created.
 pack build sample-php \
-  --builder=gcr.io/gae-runtimes/buildpacks/google-gae-22/php/builder \
+  --builder=gcp/php \
   --env GAE_APPLICATION_YAML_PATH=./app.yaml --env GOOGLE_FLEX_APPLICATION=1 \
+  --env PHP_FPM_WORKER_COUNT=5 \
   --env NGINX_SERVES_STATIC_FILES=true
+
+# To verify produced image, run the app image:
+# $ docker run --name my-sample-php
+#
+# And exec /bin/bash inside the container:
+# $ docker exec -it my-sample-php /bin/bash
+# (in app container) $ cat /layers/google.php.webconfig/webconfig/php-fpm.conf
